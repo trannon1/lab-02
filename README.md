1df226cdc7022943f7f7aff1c9624ca280aa01d4
Bryan Tran <tran_b1@denison.edu>
2019-12-04 09:45:05 -0800

    Finished first feature

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# lab-02
+# lab-
+
+Number and name of feature: Feature #1: Display images
+
+Estimate of time needed to complete: 30 minutes
+
+Start time: 9:18am
+
+Finish time: 9:44am
+
+Actual time needed to complete: 26 minutes
+
+
+Number and name of feature: Feature #2: Filter images
+
+Estimate of time needed to complete: 30 minutes - 1 hour
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
+
+
+Number and name of feature: Feature #3: Style the application
+
+Estimate of time needed to complete: 30 minutes
+
+Start time: _____
+
+Finish time: _____
+
+Actual time needed to complete: _____
---
+++ js/app.js
@@ -0,0 +1,44 @@
+const animalArray = [];
+
+function Animal(animalObj){
+  this.image_url = animalObj.image_url;
+  this.title = animalObj.title;
+  this.description = animalObj.description;
+  this.keyword = animalObj.keyword;
+  this.horns = animalObj.horns;
+
+  animalArray.push(this);
+}
+
+Animal.prototype.render = function(){
+    // make a template
+    const myTemplate = $('#animal-template').html();
+  
+    // make a new section
+    const $newSection = $('<section></section>');
+  
+    // put the template html into my new section
+    $newSection.html(myTemplate);
+  
+    // find the h2 and fill it with the name
+    $newSection.find('h2').text(this.title);
+
+    // find the h3 tag and fill with horn
+    $newSection.find('h3').text("This animal has " + this.horns + " horn/horns.");
+  
+    // find the img and fill the src and alt
+    $newSection.find('img').attr('src', this.image_url);
+    $newSection.find('img').attr('alt', this.title);
+  
+    // find the p tag and fill with description
+    $newSection.find('p').text(this.description);
+  
+    $('main').append($newSection);
+  }
+
+  $( document ).ready($.get('data/page-1.json', data => {
+        data.forEach(animal => {
+      new Animal(animal).render();
+    })
+  })
+  );
